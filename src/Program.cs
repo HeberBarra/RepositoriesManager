77e6de89d018715d0a8e0479fdf72f24f82860b7
Heber F Barra <heberferreirabarra@gmail.com>
2025-03-05 21:59:45 -0300

    [ DOC ] Added missing copyright headers

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,11 @@
-﻿using RepositoriesManager.configurator;
+﻿// Copyright (C) 2025 Heber Ferreira Barra.
+// Licensed under the Massachusetts Institute of Technology (MIT) License.
+// You may obtain a copy of the license at:
+// https://choosealicense.com/licenses/mit/
+// A short and simple permissive license with conditions only requiring preservation of copyright and license notices.
+// Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+
+using RepositoriesManager.configurator;
 using RepositoriesManager.configurator.information;
 
 Configurator configurator = new();
